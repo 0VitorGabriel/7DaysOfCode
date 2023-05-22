77575e3bab6063e35b2fd62e363c0ee05e35dc36
0VitorGabriel <victoropo64@outlook.com>
2023-05-22 08:01:14 -0400

    exercicio 2 finalizado

---
+++ 2day/script.js
@@ -0,0 +1,5 @@
+let nome = prompt('Qual o seu nome?')
+let idade = prompt('Qual anos você tem?')
+let linguagemAprendendo = prompt('Qual linguagem de programação você está estudando?')
+
+alert(`Olá ${nome}, você tem ${idade} anos e já esta aprendendo ${linguagemAprendendo}`)
