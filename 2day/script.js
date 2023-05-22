5f1d5644f5382275ff94bd27954a0c584ca6bbc5
0VitorGabriel <victoropo64@outlook.com>
2023-05-22 08:02:01 -0400

    exercicio opcional concluido

--- 2day/script.js
+++ 2day/script.js
@@ -3,3 +3,11 @@ let idade = prompt('Qual anos você tem?')
 let linguagemAprendendo = prompt('Qual linguagem de programação você está estudando?')
 
 alert(`Olá ${nome}, você tem ${idade} anos e já esta aprendendo ${linguagemAprendendo}`)
+
+let gostaAprender = prompt(`Você gosta de estudar ${linguagemAprendendo}? Responda com o número 1 para SIM ou 2 para NÃO.`)
+
+if (gostaAprender == 1) {
+    alert(`Muito bom! Continue estudando ${linguagemAprendendo} e você terá muito sucesso.`)
+} else if (gostaAprender == 2) {
+    alert('Ahh que pena... Já tentou aprender outras linguagens?')
+}
